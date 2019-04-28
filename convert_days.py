910748e51343761ff1d8bd2b662b9453fd9d1f5e
Bill <appdevelopingtoday@gmail.com>
2019-04-28 10:02:02 -0700

    Convert days program in progress

--- convert_days.py
+++ convert_days.py
@@ -1,10 +1,15 @@
+#Guillermo Mejia
+#Day converter program
+
 daysInput = int(input("How many days?"))
 years = daysInput//365
-months = daysInput%365/30
-weeks =  months/7
+months = daysInput%365//30
+weeks = daysInput%365%30
 days = weeks%7
+test = daysInput%365%30%7
 
 print(years," years.")
 print(months," months.")
 print(weeks," weeks.")
 print(days," days.")
+print(test)
